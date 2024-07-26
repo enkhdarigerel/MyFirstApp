6c9ad1d36762515c50ac51979296b27f149971a0
enkhdarigerel <enkhdarigerel@gmail.com>
2024-07-26 12:50:19 -0400

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by scholar on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var textTitle = "What is your name?"
+    @State private var name = " "
+    
+    var body: some View {
+        
+        
+        VStack {
+            Text(textTitle)
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                Button("Submit Name") {
+                    textTitle = "Welcome, \(name)!!!"
+                }
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.teal)
+            .padding()
+            
+        }
+    }
+
+    #Preview {
+        ContentView()
+    }
+
